3dc8b30b37a1fcfe694ce237ff5b2f216f42c152
Zarathustra-skip <57850472+Zarathustra-skip@users.noreply.github.com>
2021-09-01 16:07:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,18 +2,18 @@ from apscheduler.jobstores.redis import RedisJobStore
 from flask_apscheduler.auth import HTTPBasicAuth
 
 class Config(object):
-    # �洢��ʱ����Ĭ���Ǵ洢���ڴ��У�
-    SCHEDULER_JOBSTORES = {'redis': RedisJobStore(host='180.76.160.45', password='admini')}
-    # ����ʱ����ʱ����һ�»ᵼ�¶�ʱ�����ʱ�����
+    # 存储定时任务（默认是存储在内存中）
+    SCHEDULER_JOBSTORES = {'redis': RedisJobStore(host='x.x.x.x', password='xxx')}
+    # 设置时区，时区不一致会导致定时任务的时间错误
     SCHEDULER_TIMEZONE = 'Asia/Shanghai'
-    # һ��Ҫ����API���ܣ������ſ�����api�ķ�ʽȥ�鿴���޸Ķ�ʱ����
+    # 一定要开启API功能，这样才可以用api的方式去查看和修改定时任务
     SCHEDULER_API_ENABLED = True
-    # apiǰ׺��Ĭ����/scheduler��
+    # api前缀（默认是/scheduler）
     SCHEDULER_API_PREFIX = '/scheduler'
-    # ��������ִ�ж�ʱ�����������
+    # 配置允许执行定时任务的主机名
     SCHEDULER_ALLOWED_HOSTS = ['*']
-    # auth��֤��Ĭ���ǹرյģ�
+    # auth验证。默认是关闭的，
     SCHEDULER_AUTH = HTTPBasicAuth()
-    # ���ö�ʱ�����ִ������Ĭ�������ִ������Ϊ10���̳߳أ�
+    # 设置定时任务的执行器（默认是最大执行数量为10的线程池）
     SCHEDULER_EXECUTORS = {'default': {'type': 'threadpool', 'max_workers': 10}}
-    # ����flask-apscheduler������־��¼����nameΪapscheduler.scheduler��apscheduler.executors.default�������Ҫ������־������Ҫ�Դ���־��¼����������
+    # 另外flask-apscheduler内有日志记录器。name为apscheduler.scheduler和apscheduler.executors.default。如果需要保存日志，则需要对此日志记录器进行配置
